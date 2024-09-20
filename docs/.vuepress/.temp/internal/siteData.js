90c0b8f381bee4b369e8b8c1aeea051549e53cc3
SunzheTR <sunzhe1997@gmail.com>
2024-09-20 15:15:48 +0800

    feat：增加谷歌广告功能

--- docs/.vuepress/.temp/internal/siteData.js
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -1,4 +1,4 @@
-export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"ChainSmoker\",\"description\":\"Java全栈知识体系\",\"head\":[[\"link\",{\"rel\":\"icon\",\"href\":\"/images/favicon.png\"}]],\"locales\":{}}")
+export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"ChainSmoker\",\"description\":\"Java全栈知识体系\",\"head\":[\"script\",{\"async\":true,\"src\":\"https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5945083129646506\",\"crossorigin\":\"anonymous\"},[\"link\",{\"rel\":\"icon\",\"href\":\"/images/favicon.png\"}]],\"locales\":{}}")
 
 if (import.meta.webpackHot) {
   import.meta.webpackHot.accept()
